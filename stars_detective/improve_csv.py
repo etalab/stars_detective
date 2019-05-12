e6b887c99ebbfaf83125d25ac0747cf92d735948
Pavel <pavel.soriano@data.gouv.fr>
2019-05-12 15:23:22 +0200

    Added script to improve data csv

---
+++ stars_detective/improve_csv.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import sys
+
+
+
+from utils import try_toget_format_wrap
+
+
+if __name__ == '__main__':
+    n_cores = sys.argv[1]
+
+    resources_df = pd.read_csv("input_files/resources.csv", sep=";")
+
+
+    try_toget_format_wrap(resources_df, n_cores=n_cores)
